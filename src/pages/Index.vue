8c03777238be3670538aab8758d0008fd4faf02b
Bishwo Dahal <bishwodahal123@gmail.com>
2025-03-06 18:57:16 -0600

    added meta tags for index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -41,10 +41,51 @@ import SectionViewDonors from '~/components/sections/ViewDonors'
 import SectionCompanyProfileLists from '~/components/sections/CompanyProfileLists'
 import SectionImageCarousel from '~/components/sections/ImageCarousel'
 
+const mainPageExcerpt = "We For Humanity is a Nepal-based nonprofit organization dedicated to helping those in need through donations, volunteering, and community support.";
+const mainPageKeywords = "We For Humanity, Nepal NGO, nonprofit organization, charity in Nepal, humanitarian aid, donations, volunteering, fundraising, community service, social impact, food distribution, education support, healthcare assistance, social welfare, support the needy, make a difference";
+const mainPageImage = "/images/thumbnail/first_annual_meeting.JPG"
 
 export default {
   metaInfo: {
-    title: 'We for Humanity: Charikot, Nepal'
+    title: 'We for Humanity: Charikot, Nepal',
+    meta:[
+    {
+          name:"developer",
+          content:"Bishwo Dahal"
+        }, 
+        {
+          property:'description',
+          content:mainPageExcerpt
+        },
+        {
+          property:'keywords',
+          content:mainPageKeywords
+        },
+        {
+          property:'og:title',
+          content:`We For Humanity: Charikot, Nepal`
+        },
+        {
+          property:'og:description',
+          content:mainPageExcerpt
+        },
+        {
+          property:'og:image',
+          content:mainPageImage
+        },
+        {
+          property:'twitter:title',
+          content:`We For Humanity: Charikot, Nepal`
+        },
+        {
+          property:'twitter:description',
+          content:mainPageExcerpt
+        },
+        {
+          property:'twitter:image',
+          content:mainPageImage
+        }
+    ]
   },
   components: {
     PageHeader,
