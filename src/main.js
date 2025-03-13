89f133e8d6fb2de7ac7cb5b03af3a632604a433e
Bishwo Dahal <bishwodahal123@gmail.com>
2025-03-13 08:56:23 -0500

    added google analytics

--- src/main.js
+++ src/main.js
@@ -8,6 +8,22 @@ import '~/assets/scss/main.scss'
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
 export default function (Vue, { router, head, isClient }) {
+
+    // Add Google Analytics script
+    head.script.push({
+      src: "https://www.googletagmanager.com/gtag/js?id=G-5ZPCG88RLL",
+      async: true
+    });
+
+    head.script.push({
+      innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-5ZPCG88RLL');
+      `,
+      type: "text/javascript"
+    });
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
